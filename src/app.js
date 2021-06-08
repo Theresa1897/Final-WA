c8a2447a5cf194a1e1b3d02f49647c77ab5c3a15
Theresa1897 <82607715+Theresa1897@users.noreply.github.com>
2021-06-08 01:17:47 -0700

    Added search engine

--- src/app.js
+++ src/app.js
@@ -44,7 +44,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "f31f167d6e8ea7b22fba3adb5bdbb2b7";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "f31f167d6e8ea7b22fba3adb5bdbb2b7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("New York");
